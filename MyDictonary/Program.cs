1d54faae0f227b0c57dcf1bfc1ed0d837bc58b35
Mehmet Ali <kahilmehmetali@gmail.com>
2021-01-29 22:40:29 +0300

    son

---
+++ MyDictonary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictonary = new MyDictionary<int, string>();
+            myDictonary.Add(5, "Ankara");
+        }
+    }
+}
