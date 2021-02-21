d211a0660569e851277a9481815c613d1178f546
Mehmet Ali <kahilmehmetali@gmail.com>
2021-02-21 07:41:25 +0300

    21subat1

---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer1 = new Customer();
+            customer1.City = "Ankara";
+            customer1.Id = 1;
+            customer1.FirstName = "Engin";
+            customer1.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                City = "İstanbul",
+                FirstName = "Derin",
+                LastName = "Demir"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // InterfacesIntro();
+
+            // Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal()
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager personManager = new PersonManager();
+
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                FirstName = "Mehmet Ali",
+                LastName = "Kahil",
+                Address = "Çiğdede Mah."
+            };
+
+            Student student = new Student()
+            {
+                Id = 2,
+                FirstName = "Engin",
+                LastName = "Demirog",
+                Departmant = "Software"
+            };
+
+            personManager.Add(student);
+            personManager.Add(customer);
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string Address { get; set; }
+
+
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+
+}
