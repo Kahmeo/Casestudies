2a64ff56bdcc900e2fd2e39bea31b9a8dbf1a950
Mehmet Ali <kahilmehmetali@gmail.com>
2021-02-22 09:38:29 +0300

    22subat

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,24 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            MernisServiceReference.KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula(
+                Convert.ToInt64(customer.NationalityId), customer.FirsName.ToUpper(), customer.LastName.ToUpper(), 
+                customer.DateOfBirth.Year);
+        }
+
+        public void Save(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    { /*Senaryo
+       Kahve dükkanları için müşteri yönetimi yapan bir sistem yazmak istiyoruz.
+        Starbuks ve Nero firması için çalışıyoruz.
+        iki firma da müşterilerini veri tabanına kaydetmek istiyor.
+        Starbucks müşterileri kaydederken, mutlaka mernis doğrulaması istiyor. Nero müşteri kaydederken böyle bir şey istemiyor.
+        Starbucks müşteriler için her kahve alımında yıldız kazandırmak istiyor.
+       */
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer 
+            { 
+                DateOfBirth = new DateTime(1998, 12, 5), 
+                FirsName = "Mehmet Ali", 
+                LastName = "Kahiloğulları", 
+                NationalityId = "44701228660"
+            });
+            Console.ReadLine();
+        }
+    }
+}
